74e45a7a4e1bd74b0cd36e10fe8badf811ad0aaf
Layton <laytonb@hotmail.co.uk>
2020-04-07 15:59:31 +0100

    to be tested

--- builder/main.ts
+++ builder/main.ts
@@ -334,8 +334,13 @@ const PaladinAura = (function() {
           setAttr(p.id, 'paladin_buff', p.chaBonus.toString());
         }
         const distLimit = (p.radius / unitsPerSquare) * pixelsPerSquare;
-        const xDist = Math.abs(t.get('left') - p.left);
-        const yDist = Math.abs(t.get('top') - p.top);
+        const tokenSizeAdjust =
+          t.get('width') == pixelsPerSquare
+            ? 0
+            : (Math.floor(t.get('width') / pixelsPerSquare) - 1) *
+              (pixelsPerSquare / 2);
+        const xDist = Math.abs(t.get('left') - p.left) - tokenSizeAdjust;
+        const yDist = Math.abs(t.get('top') - p.top) - tokenSizeAdjust;
         const distTotal =
           xDist >= yDist ? distCalc(xDist, yDist) : distCalc(yDist, xDist);
         if (
